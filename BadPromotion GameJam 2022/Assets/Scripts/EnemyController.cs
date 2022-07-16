88bf52653706552acd0d03f26926f4e081fb37fe
ismaUPC <itejada.upc@gmail.com>
2022-07-16 12:15:19 +0200

    Enemy path attak

--- BadPromotion GameJam 2022/Assets/Scripts/EnemyController.cs
+++ BadPromotion GameJam 2022/Assets/Scripts/EnemyController.cs
@@ -5,10 +5,16 @@ using UnityEngine.AI;
 
 public class EnemyController : MonoBehaviour
 {
+    public GameObject player;
     [SerializeField] GameObject target;
     NavMeshAgent navAgent;
+    public int alertArea;
 
-    [SerializeField] int life = 20;
+    enum TypeEnemy { walker, flying };
+    [SerializeField] TypeEnemy typeEnemy;
+
+
+    [SerializeField] int life = 2;
     [SerializeField] int damage = 5;
 
     [SerializeField] float attackCooldown = 1.5f;
@@ -16,30 +22,35 @@ public class EnemyController : MonoBehaviour
 
     bool firstFrame = true;
 
+    public GameObject[] waypoints;
+    int patrolWP = 4;
+
+
     // Start is called before the first frame update
     void Start()
     {        
         navAgent = GetComponent<NavMeshAgent>();
+        target = waypoints[0];
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (life <= 0)
+        if (life <= 0) Die();
+        
+        float distance = navAgent.remainingDistance;
+
+        //Patrol
+        if ((!navAgent.pathPending && distance < 0.5f) && Vector3.Distance(transform.position, player.transform.position) > alertArea) PatrolPattern();
+        else
         {
-            Die();
+            target = player;
+            Attak(distance);
         }
+    }
 
-        float distance = Vector3.Distance(target.transform.position, transform.position);
-        if (distance > 5)
-            return;
-
-        if (firstFrame)
-        {
-            navAgent.destination = target.transform.position;
-            navAgent.stoppingDistance = 2.1f;
-        }
-
+    private void Attak(float distance)
+    {
         if (lasAttackCount > 0)
         {
             lasAttackCount -= Time.deltaTime;
@@ -48,11 +59,10 @@ public class EnemyController : MonoBehaviour
         {
             if (distance < navAgent.stoppingDistance)
             {
-                Attack();
+                Hit();
                 lasAttackCount = attackCooldown;
             }
         }
-
         if (navAgent.destination != target.transform.position)
         {
             navAgent.destination = target.transform.position;
@@ -68,7 +78,7 @@ public class EnemyController : MonoBehaviour
         }
     }
 
-    void Attack()
+    void Hit()
     {
         target.GetComponent<PlayerController>().TakeDamage(damage);
         Debug.Log("Attacked!");
@@ -77,5 +87,15 @@ public class EnemyController : MonoBehaviour
     void Die()
     {
         Destroy(gameObject);
+    }
+    void PatrolPattern()
+    {
+        patrolWP = (patrolWP + 1) % waypoints.Length;
+        Seek(waypoints[patrolWP].transform.position);
+    }
+
+    void Seek(Vector3 targetPosition)
+    {
+        navAgent.destination = targetPosition;
     }
 }
