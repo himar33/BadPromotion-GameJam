ab28ec367088b1e827b591de4c57abfa3442ad32
MagiX7 <DavidGonzalez2001-9@hotmail.com>
2022-07-17 17:13:19 +0200

    lvl 2 and 1

--- BadPromotion GameJam 2022/Assets/Scripts/BallChecker.cs
+++ BadPromotion GameJam 2022/Assets/Scripts/BallChecker.cs
@@ -6,12 +6,16 @@ public class BallChecker : MonoBehaviour
 {
     public BallController ball;
 
+    void Update()
+    {
+        transform.position = new Vector3(ball.transform.position.x, transform.position.y, transform.position.z);
+    }
+
     private void OnTriggerEnter(Collider other)
     {
         if (other.tag == "Player")
         {
             ball.allowMove = true;
-            //ball.GetComponent<Rigidbody>().isKinematic = false;
         }
     }
 
@@ -20,7 +24,6 @@ public class BallChecker : MonoBehaviour
         if (other.tag == "Player")
         {
             ball.allowMove = false;
-            //ball.GetComponent<Rigidbody>().isKinematic = true;
         }
     }
 
